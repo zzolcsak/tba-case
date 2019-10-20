748b8885f751e12e408d9074dcfbcb53cac69cd4
Zolcsak Zsolt <zolcsak.zsolt@accenture.com>
2019-10-20 19:40:40 +0300

    Added moving forever in the direction last specified

--- vehicle.presenter/src/main/java/com/zzolcsak/vehicle/presenter/Application.java
+++ vehicle.presenter/src/main/java/com/zzolcsak/vehicle/presenter/Application.java
@@ -5,7 +5,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,7 +20,7 @@ import com.zzolcsak.vehicle.presenter.bean.VehicleBean;
 
 @SpringBootApplication
 public class Application {
-	private Map<VehicleBean, ScheduledExecutorService> vehicleMovers;
+	private static Map<Long, ScheduledExecutorService> vehicleMovers = new HashMap<>();
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -29,15 +31,18 @@ public class Application {
 				System.out.print("Next move ('help' for list of moves):");
 				nextMove = scanner.nextLine();
 				if ("help".equals(nextMove)) {
-					System.out.println("Available moves: help, exit, create, refresh, moveUp id, moveDown id, moveLeft id, moveRight id");
+					System.out.println(
+							"Available moves: help, exit, create, refresh, moveUp id, moveDown id, moveLeft id, moveRight id");
 				} else if ("create".equals(nextMove)) {
 					createNew();
 				} else if (nextMove.trim().startsWith("move")) {
 					try {
 						int indexOfSpace = nextMove.indexOf(' ');
 						String direction = nextMove.substring(4, indexOfSpace);
-						int id = Integer.parseInt(nextMove.substring(indexOfSpace + 1));
-						move(id, direction.substring(0,1).toUpperCase() + direction.substring(1));
+						long id = Long.parseLong(nextMove.substring(indexOfSpace + 1));
+						String capitalizedDirection = direction.substring(0, 1).toUpperCase() + direction.substring(1);
+						move(id, capitalizedDirection);
+						moveForever(id, capitalizedDirection);
 					} catch (NumberFormatException e) {
 						System.err.println("Error: '" + nextMove
 								+ "' couldn't be interpreted. Usage: moveUp vehicleId. Example: moveUp 1");
@@ -47,13 +52,26 @@ public class Application {
 		}
 	}
 
-	private static void move(int id, String direction) {
-		Map<String, Integer> uriVariables = new HashMap<>();
-		uriVariables.put("id", id);
-		new RestTemplate().getForEntity("http://localhost:8080/vehicles/{id}/move" + direction, String.class, uriVariables)
-				.getBody();
+	private static void moveForever(long id, String capitalizedDirection) {
+		ScheduledExecutorService executor = vehicleMovers.get(id);
+		if (executor != null) {
+			executor.shutdownNow();
+		}
+		createNewMover(id, capitalizedDirection);
+	}
 
-		System.out.println("Moved " + direction.toLowerCase() + " " + id);
+	private static void createNewMover(long id, String capitalizedDirection) {
+		ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+		executor.scheduleAtFixedRate(() -> move(id, capitalizedDirection), 1, 1, TimeUnit.SECONDS);
+		vehicleMovers.put(id, executor);
+	}
+
+	private static void move(long id, String direction) {
+		Map<String, Long> uriVariables = new HashMap<>();
+		uriVariables.put("id", id);
+		new RestTemplate()
+				.getForEntity("http://localhost:8080/vehicles/{id}/move" + direction, String.class, uriVariables)
+				.getBody();
 	}
 
 	private static void createNew() {
